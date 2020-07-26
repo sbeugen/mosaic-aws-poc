390f73ac2e21f157cd205b9cc9c683e8ad638614
Eugen Sawitzki <eugen.sawitzki@comsysto.com>
2020-07-26 18:33:37 +0200

    deploying all resources

--- lambda/tailor-lambda/src/fetchTemplate.js
+++ lambda/tailor-lambda/src/fetchTemplate.js
@@ -3,7 +3,7 @@ const AWS = require("aws-sdk");
 module.exports = (request, parseTemplate) => {
   const templateName = request.apiGateway.event.pathParameters.template;
   const s3 = new AWS.S3({apiVersion: '2006-03-01'});
-  const s3BucketName = process.env.TEMPLATE_BUCKET_NAME;
+  const s3BucketName = process.env.TEMPLATES_BUCKET_NAME;
 
   return new Promise((resolve, reject) => {
     if (templateName === "favicon.ico") resolve("");
